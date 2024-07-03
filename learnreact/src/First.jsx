051c231c1127ee98762c63a697bbed8e4837726b
rohitjainfsl <rohit@fullstacklearning.com>
2024-07-03 10:47:39 +0530

    day 3 before lunch

---
+++ learnreact/src/First.jsx
@@ -0,0 +1,9 @@
+function First() {
+  return (
+    <>
+      <h1>My first react project</h1>
+      <h2>Yeh nahi chalega</h2>
+    </>
+  );
+}
+export default First;
---
+++ learnreact/src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import First from "./First.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<First />);
